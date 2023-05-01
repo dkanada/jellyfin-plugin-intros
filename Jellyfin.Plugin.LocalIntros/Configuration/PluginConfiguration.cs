98a61acfc6875a408a8f88d5c0499a7ff09659e2
Brian C. Arnold <brian.arnold@spiderrock.net>
2023-05-01 13:08:18 -0400

    Added "Add All" button for #4.

--- Jellyfin.Plugin.LocalIntros/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.LocalIntros/Configuration/PluginConfiguration.cs
@@ -2,55 +2,54 @@ using System;
 using System.Collections.Generic;
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.LocalIntros.Configuration
+namespace Jellyfin.Plugin.LocalIntros.Configuration;
+
+public class IntroPluginConfiguration : BasePluginConfiguration
 {
-    public class IntroPluginConfiguration : BasePluginConfiguration
-    {
-        public string Local { get; set; } = string.Empty;
+    public string Local { get; set; } = string.Empty;
 
-        public List<IntroVideo> DetectedLocalVideos { get; set; } = new List<IntroVideo>();
+    public List<IntroVideo> DetectedLocalVideos { get; set; } = new List<IntroVideo>();
 
-        public List<Guid> DefaultLocalVideos { get; set; } = new List<Guid>();
+    public List<Guid> DefaultLocalVideos { get; set; } = new List<Guid>();
 
-        public List<TagIntro> TagIntros { get; set; } = new List<TagIntro>();
-        public List<GenreIntro> GenreIntros { get; set; } = new List<GenreIntro>();
-        public List<StudioIntro> StudioIntros { get; set; } = new List<StudioIntro>();
+    public List<TagIntro> TagIntros { get; set; } = new List<TagIntro>();
+    public List<GenreIntro> GenreIntros { get; set; } = new List<GenreIntro>();
+    public List<StudioIntro> StudioIntros { get; set; } = new List<StudioIntro>();
 
-    }
+}
 
-    public class IntroVideo
-    {
-        public string Name { get; set; }
+public class IntroVideo
+{
+    public string Name { get; set; }
 
-        public Guid ItemId { get; set; }
-    }
+    public Guid ItemId { get; set; }
+}
 
-    public interface ISpecialIntro
-    {
-        Guid IntroId { get; set; }
-        int Precedence { get; set; }
-        int Prevalence { get; set; }
-    }
+public interface ISpecialIntro
+{
+    Guid IntroId { get; set; }
+    int Precedence { get; set; }
+    int Prevalence { get; set; }
+}
 
-    public class TagIntro : ISpecialIntro
-    {
-        public Guid IntroId { get; set; }
-        public string TagName { get; set; }
-        public int Precedence { get; set; }
-        public int Prevalence { get; set; }
-    }
-    public class GenreIntro : ISpecialIntro
-    {
-        public Guid IntroId { get; set; }
-        public string GenreName { get; set; }
-        public int Precedence { get; set; }
-        public int Prevalence { get; set; }
-    }
-    public class StudioIntro : ISpecialIntro
-    {
-        public Guid IntroId { get; set; }
-        public string StudioName { get; set; }
-        public int Precedence { get; set; }
-        public int Prevalence { get; set; }
-    }
+public class TagIntro : ISpecialIntro
+{
+    public Guid IntroId { get; set; }
+    public string TagName { get; set; }
+    public int Precedence { get; set; }
+    public int Prevalence { get; set; }
+}
+public class GenreIntro : ISpecialIntro
+{
+    public Guid IntroId { get; set; }
+    public string GenreName { get; set; }
+    public int Precedence { get; set; }
+    public int Prevalence { get; set; }
+}
+public class StudioIntro : ISpecialIntro
+{
+    public Guid IntroId { get; set; }
+    public string StudioName { get; set; }
+    public int Precedence { get; set; }
+    public int Prevalence { get; set; }
 }
